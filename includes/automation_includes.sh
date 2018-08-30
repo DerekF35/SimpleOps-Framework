d30bdeb9d169c401af5e809ae484ac6088b1d7b8
Derek Frankhouser <derek.frankhouser@gmail.com>
2018-08-30 18:47:32 -0400

    Improve local script running

--- includes/automation_includes.sh
+++ includes/automation_includes.sh
@@ -15,25 +15,50 @@ function runScript(){
 	run_cmd "${2}" "${3}" |& tee -a $log
 }
 
+function run_remote_script(){
+	echo "Running remote script.."
+	run_cmd "${1}" "${2}" "X"
+}
+
 function run_cmd(){
 	set +e
 	if [ "${1}" == "local" ]
 	then
 		cd ${SCRIPT_LOCATION}
 		${LOCAL_PRE_COMMAND}
-		eval "${2}"
+		echo "Beginning command run. (${2})..."
+		eval ${2}
 		writeExitCode "$?"
 	else
+		echo
+		echo "################################"
+		echo " Server: ${1}"
+		echo " Server: ${2}"
+		echo "################################"
+		echo
 		source ${REMOTE_SERVER_CONFIGS}/${1}.sh
 		${SSH_COMMAND} <<ENDSSH
 		cd ${SCRIPT_LOCATION}
 		${PRE_COMMAND}
-		echo "Beginning command run. (${2})..."
-		echo ""
+		echo
+		echo "Beginning command run...."
+		echo
 		${2}
-		exit $?
+		export myExitCode=\${?}
+		echo "### Exit Code: \$myExitCode"
+		echo
+		exit \$myExitCode
 ENDSSH
-		writeExitCode "$?"
+		myExitCode="${?}"
+		if [ "${3}" == "X" ]
+		then
+			if [ "${myExitCode}" != "0" ]
+			then
+				exit ${myExitCode}
+			fi
+		else
+			writeExitCode "$myExitCode"
+		fi
 	fi
 	set -e
 }
