8ff7422e09328c79dca7116ed53ab724fd638b8a
Derek <derek.frankhouser@gmail.com>
2022-06-01 20:23:37 -0400

    Always notify on failure unless notification is 'none'

--- script_run.sh
+++ script_run.sh
@@ -40,7 +40,7 @@ arg_chk_pass=1
 if [ -z ${JOBNAME+x} ]; then echo "JOBNAME is unset. Use -t <input>"; arg_chk_pass=0; fi
 if [ -z ${SERVER+x} ]; then echo "SERVER is unset. Use -s <input>"; arg_chk_pass=0; fi
 if [ -z ${COMMAND+x} ]; then echo "COMMAND is unset. Use -c <input>"; arg_chk_pass=0; fi
-if [ -z ${NOTIFICATION+x} ]; then echo "NOTIFICATION is unset. Use -n [none|all|failure|grep=<pattern>]"; arg_chk_pass=0; fi
+if [ -z ${NOTIFICATION+x} ]; then echo "NOTIFICATION is unset. Notifying on failure"; NOTIFICATION=failure; fi
 if [ "$arg_chk_pass" != "1" ]; then exit 1; fi
 
 runScript "$JOBNAME" "$SERVER" "$COMMAND"
@@ -49,10 +49,14 @@ scriptExitCode="$(readExitCode)"
 my_log=${LOG_LOCATION}/$( readDataValue ${MY_DATA_FILE} LOG )
 my_log_link=${WEB_LOCATION}/$( readDataValue ${MY_DATA_FILE} LOG )
 
+if [ "${NOTIFICATION}" != "none" ] && [ "${scriptExitCode}" != "0" ]; then
+  slackNotify ${JOBNAME} "$(getResultText $scriptExitCode )" ${my_log_link};
+  exit 0
+fi
+
 case $NOTIFICATION in
 	none) echo "No notifcation set";;
 	all) slackNotify ${JOBNAME} "$( getResultText $scriptExitCode )" ${my_log_link};;
-	failure) if [ "${scriptExitCode}" != "0" ]; then slackNotify ${JOBNAME} "$(getResultText $scriptExitCode )" ${my_log_link}; fi;;
   grep=*) if [ "$( grep -P "${NOTIFICATION//grep=/}" ${my_log} | wc -l )" != "0" ]; then slackNotify ${JOBNAME} "NOTIFICATION" ${my_log_link} ; fi;;
 esac
 
